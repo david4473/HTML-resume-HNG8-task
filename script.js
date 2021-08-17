fbd85d08419a1373e060c741109fc2f98cef38f9
david <omotayodavid9@gmail.com>
2021-08-17 11:11:03 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const printName = () =>{
+
+    const firstName = 'David';
+    const LastName = 'Omotayo';
+    const fullName = `${firstName} ${LastName}` 
+    console.log(fullName);
+}
+
+printName();
+
+
+
